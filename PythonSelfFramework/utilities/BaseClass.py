51f213595b72ae284198db8d89db86fd587d4628
rbuma8426@gmail.com <rbuma8426@gmail.com>
2021-09-21 17:57:19 +0700

    BaseClass

--- PythonSelfFramework/utilities/BaseClass.py
+++ PythonSelfFramework/utilities/BaseClass.py
@@ -1,7 +1,12 @@
 import pytest
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
 
 
 @pytest.mark.usefixtures("setup")
 class BaseClass:
-    pass
+    def verify_link_presence(self, text):
+        element = WebDriverWait(self.driver, 10).until(
+            expected_conditions.presence_of_element_located((By.LINK_TEXT, text)))
 
